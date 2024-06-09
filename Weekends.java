d5d58c2f6f362fad401f6c74c04bc7d557e42cc0
Kamaljeet <kamaljeetkamaljeet548@gmail.com>
2024-06-09 23:45:13 +0530

    Update Weekends.java

--- Weekends.java
+++ Weekends.java
@@ -18,3 +18,53 @@ class Solution {
             return nums[0];
     }
 }
+
+
+
+/// FLOOR function:
+
+ public static void Floor(int[] arr,int target){
+        int low=0;
+      int high=4;
+     int  floor=-1;
+      while(low<=high){
+           int mid = (low+high)/2;
+          if(arr[mid]==target){
+              floor=arr[mid];
+              break;
+          }
+          else if(arr[mid]>target){
+              high=mid - 1;
+          }else if(arr[mid]<target){
+              floor=arr[mid];
+              low=mid + 1;
+          }
+      }
+          System.out.println("Floor : "+floor);
+    }
+
+
+
+//CEIL function :
+
+    public static void Ceil(int[] arr,int target){
+        int low=0;
+        int high=4;
+        int  ceil=-1;
+      while(low<=high){
+           int mid = (low+high)/2;
+          if(arr[mid]==target){
+              ceil=arr[mid];
+              break;
+          }
+          else if(arr[mid]>target){
+              ceil=arr[mid];
+              high=mid - 1;
+          }else if(arr[mid]<target){
+              
+              low=mid + 1;
+          }
+      }
+          System.out.println("Ceil : "+ceil);
+    }
+
