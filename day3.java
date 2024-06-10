712ce0a9421a848c6418ea75278b0b562eace707
Kamaljeet <kamaljeetkamaljeet548@gmail.com>
2024-06-10 15:23:00 +0530

    Update day3.java

--- day3.java
+++ day3.java
@@ -20,3 +20,49 @@ class HelloWorld {
         System.out.println("No. of factors are : "+count);
     }
 }
+
+//Prime number or not : 
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int count =0;
+        for(int i=1;i<Math.sqrt(num);i++){
+            if(num%i==0 ){
+                if(i != num/i){
+                    count+=2;
+                }else if (i == num/i){
+                    count++;
+                }
+            }
+        }
+        if(count==2){
+            System.out.println("Prime");}
+        else System.out.println("Not prime");
+    }
+}
+
+
+//Count Factors using seivefo..
+
+
+class Solution {
+    public int countPrimes(int n) {
+        boolean prime[] = new boolean[n + 1];
+        for (int i = 0; i <= n; i++)
+            prime[i] = true;
+        for (int p = 2; p * p <= n; p++) {
+            if (prime[p] == true) {
+                for (int i = p * p; i <= n; i += p)
+                    prime[i] = false;
+            }
+        }
+        int count=0;
+        // Print all prime numbers
+        for (int i = 2; i <n; i++) {
+            if (prime[i] == true)
+                count++;
+    }
+    return (count);
+}
+}
